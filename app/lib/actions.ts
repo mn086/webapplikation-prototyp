f45b5c82d56a0eac1bf161f23675d28462d1070c
mn086 <167792384+mn086@users.noreply.github.com>
2025-05-19 21:15:01 +0200

    Implementiere Serverseitige Formvalidierung der edit-form-Komponente.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -75,13 +75,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Verwende Zod, um die erwarteten Typen zu aktualisieren
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Fehlende Felder. Aktualisierung der Rechnung fehlgeschlagen.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
@@ -91,8 +99,9 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
   } catch (error) {
-    // den Fehler vorerst in der Konsole loggen
-    console.error(error);
+    return {
+      message: 'Datenbankfehler: Rechnung konnte nicht aktualisiert werden.',
+    };
   }
  
   revalidatePath('/dashboard/invoices');
